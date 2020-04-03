f651f5b1bc5528883e7c1cdef259cfb1b79b8848
Joe Attardi <jattardi@gmail.com>
2020-04-02 21:19:14 -0400

    Add last updated time

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { format } from 'date-fns';
+
 import StatisticTile from './StatisticTile';
 
 import styles from './Main.module.scss';
@@ -7,6 +9,7 @@ import styles from './Main.module.scss';
 export default function Main({ totals }) {
   return (
     <main id={styles.main}>
+      <h2 className={styles['last-updated']}>Last updated {format(totals.updated, 'MMMM d, yyyy h:mm a')}</h2>
       <div id={styles.totals}>
         <StatisticTile label="Total Cases" value={totals.cases} type="cases" />
         <StatisticTile label="Deaths" value={totals.deaths} type="deaths" />
