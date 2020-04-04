0beddd865d0738a4413e6d22a25eac093f65a7ef
Joe Attardi <jattardi@gmail.com>
2020-04-03 21:11:02 -0400

    Add fixed header

--- src/countryListTable/CountryListTable.js
+++ src/countryListTable/CountryListTable.js
@@ -11,25 +11,27 @@ function getCellComponent(column, rowData, data, rowIndex) {
 
 export default function CountryListTable({ data, columns }) {
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {columns.map(column => (
-            <th key={column.property}>{column.label}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((countryData, rowIndex) => (
-          <tr key={countryData.countryInfo.iso2}>
-            {columns.map((column) => (
-              <td key={column.property}>
-                {getCellComponent(column, countryData, data, rowIndex)}
-              </td>
+    <div className={styles['table-container']}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {columns.map(column => (
+              <th key={column.property}>{column.label}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((countryData, rowIndex) => (
+            <tr key={countryData.countryInfo.iso2}>
+              {columns.map((column) => (
+                <td key={column.property}>
+                  {getCellComponent(column, countryData, data, rowIndex)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
